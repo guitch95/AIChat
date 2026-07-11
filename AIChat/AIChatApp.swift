184ad70fb5ef1d126a051e982abeb70c99be210d
Guillaume Richard <guitch95@gmail.com>
2026-07-11 11:13:51 +0200

    Initial Commit

---
+++ AIChat/AIChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatApp.swift
+//  AIChat
+//
+//  Created by Guillaume Richard on 11/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct AIChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChatTests/AIChatTests.swift
@@ -0,0 +1,19 @@
+//
+//  AIChatTests.swift
+//  AIChatTests
+//
+//  Created by Guillaume Richard on 11/07/2026.
+//
+
+import Testing
+@testable import AIChat
+
+struct AIChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
